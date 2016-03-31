a26f241f6da943e8c8e9593649abf3915c972c16
Jason Efthimiou <Jason Efthimiou>
2016-03-30 22:44:19 -0400

    Initial Push

---
+++ src/MVAMVC/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using MVAMVC.Models;
+
+namespace MVAMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Foo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> About(RegisterUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                /*
+                var nau = new ActiveUsers();
+                int i = nau.Username.Length;
+
+                nau.Username[i] = model.Email;
+                nau.Password[i] = model.Password;
+                nau.DateRegistered[i] = model.DateRegistered
+                */
+                
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This text is from HomeController.cs";
+
+            var u = new RegisterUser
+            {
+                DateRegistered = DateTime.Now
+            };
+
+
+            return View(u);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MVAMVC/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVAMVC.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(20), MinLength(2)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        [Required]
+        public DateTime DateRegistered { get; set; }
+    }
+}
